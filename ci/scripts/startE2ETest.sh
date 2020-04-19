c7ebba93e4d5b18e6242376de0e7d89ac9491976
Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>
2020-04-19 16:46:58 -0400

    CI integration
    Signed-off-by: Nishant Chauhan <4951964+NishantChauhan@users.noreply.github.com>

--- ci/scripts/startE2ETest.sh
+++ ci/scripts/startE2ETest.sh
@@ -1,4 +1,4 @@
 cd CurrencyUtilities
 export CHROME_BIN=/usr/bin/chromium-browser
 export CHROME_DRIVER=/usr/bin/chromedriver
-npm run dev-e2e
+npm run ci-e2e
--- ci/scripts/stopCurrencyCIEnv.sh
+++ ci/scripts/stopCurrencyCIEnv.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh
-
 docker-compose down
-#docker stop currency-utilities-app-container currency-utilities-server-dev-container
